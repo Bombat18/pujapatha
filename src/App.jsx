738fe82a729a5954afc087410bc4ac3f6af05326
bombat18 <ramyaranjan78@gmail.com>
2024-12-10 21:28:41 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Nav from "./component/Nav";
-import Hero from './pages/Hero';
+import Nav from "./component/Nav.jsx";
+import Hero from './pages/Hero.jsx';
 
 function App() {
  
